091e089c6b0fe96cbb4756b9960f7a6a79b8130a
Dimitar Angelov <dmapostbox@gmail.com>
2021-09-17 18:33:42 +0300

    Updated rule-based classifier to check all keywords and choose the most common one.

--- src/rule_based.py
+++ src/rule_based.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 from rule_dict import rule_dict
+from rule_dict import count_dict
 
 
 class RuleBasedClassifier(BaseEstimator, ClassifierMixin):
@@ -13,13 +14,16 @@ class RuleBasedClassifier(BaseEstimator, ClassifierMixin):
     def predict(self, x, y=None):
         return list(map(self.predict_most_likely, x))
 
-    def predict_proba(self, x, y=None):
-        return list(map(self.predict_most_likely, x))
-
     @staticmethod
     def predict_most_likely(sentence):
-        for word in sentence.split():
-            if word in rule_dict.keys():
-                if rule_dict[word]:
-                    return rule_dict[word]
-        return "inform"
+        c_d_temp = count_dict.copy()
+        for word in sentence.split():  # check over each word
+            if word in rule_dict.keys():  # if it is a keyword
+                c_d_temp[rule_dict.get(word)] += 1  # add one to that keyword
+        if max(c_d_temp.values()) != 0:  # if any keyword was found
+            print(c_d_temp)
+            print(max(c_d_temp, key=c_d_temp.get))
+            return max(c_d_temp, key=c_d_temp.get)  # return the most common one
+        else:
+            print("None")
+            return "inform"  # the most common class
--- src/rule_dict.py
+++ src/rule_dict.py
@@ -49,3 +49,21 @@ rule_dict = {
     "thank": "thankyou",
     "great": "thankyou"
 }
+
+count_dict = {
+    "ack": 0,  # acknowledgment
+    "affirm": 0,  # positive confirmation
+    "bye": 0,  # greeting at the end of the dialog
+    "confirm": 0,  # check if given information confirms to query
+    "deny": 0,  # reject system suggestion
+    "hello": 0,  # greeting at the start of the dialog
+    "inform": 0,  # state a preference or other information
+    "negate": 0,  # negation
+    "null": 0,  # noise or utterance without content
+    "repeat": 0,  # ask for repetition
+    "reqalts": 0,  # request alternative suggestions
+    "reqmore": 0,  # request more suggestions
+    "request": 0,  # ask for information
+    "restart": 0,  # attempt to restart the dialog
+    "thankyou": 0,  # express thanks
+}
