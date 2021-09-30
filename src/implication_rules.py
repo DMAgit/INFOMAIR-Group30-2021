bfed00941fb2f0e9647a3d6b53bc63a7ec73b219
Thimo <SanzuTip@gmail.com>
2021-10-01 01:00:50 +0200

    Finished implication_rules.py. We can now check ff a restaurant suggestion is suitable for agoraphobes, people in a hurry, children or tinderdates.

--- src/implication_rules.py
+++ src/implication_rules.py
@@ -1,6 +1,34 @@
-import pandas as pd
 
-res_df = pd.read_csv(r'../data/restaurant_info.csv')
+def is_busy(suggestion):
+    if suggestion.crowdedness == "busy":
+        return True
+    if suggestion.food_quality == "good" and suggestion.pricerange == "cheap":
+        return True
+    return False
 
-ex_restaurant = res_df.iloc[0]
-if restaurant.
+
+def is_long(suggestion):
+    if suggestion.length_stay == "long":
+        return True
+    if suggestion.food == "spanish":
+        return True
+    if is_busy(suggestion):
+        return True
+    return False
+
+
+def children_advised(suggestion):
+    if is_long(suggestion):
+        return False
+    return True
+
+
+def is_romantic(suggestion):
+    # crowdedness takes priority over length of stay, so if a restaurant is busy, the restaurant will never be romantic
+    if is_busy(suggestion):
+        return False
+    if is_long(suggestion):
+        return True
+
+    return False
+
